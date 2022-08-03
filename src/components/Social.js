8cc8ea3c42097bb5a79f1073f6e0bd6df8070578
bmbaron <benjamin.m.baron@gmail.com>
2022-08-03 15:39:14 +0700

    fixed typo

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import GithubIcon from '@material-ui/icons/Github'
+import GithubIcon from '@material-ui/icons/GitHub'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
 import Grid from '@material-ui/core/Grid'
 import IconButton from '@material-ui/core/IconButton'
